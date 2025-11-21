138546028dfa1f2422b712e68517b85630dd9157
Luan Mota Coleto <luan@coleto.dev>
2025-11-21 17:56:44 -0300

    feat: move firebase credentials to environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDakcmPeDILvV03GywP4eBJzsfrCreq2PY",
-  authDomain: "renting-help.firebaseapp.com",
-  projectId: "renting-help",
-  storageBucket: "renting-help.firebasestorage.app",
-  messagingSenderId: "884406517172",
-  appId: "1:884406517172:web:4274ecc09927f813b506e1",
-  measurementId: "G-8GRZHB8C6Y"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase apenas se ainda não foi inicializado
